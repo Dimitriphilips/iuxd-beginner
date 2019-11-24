48b30449a2bacffe890f5109728a0057abbce3e7
Dimitriphilips <Dimitriphilips@msn.com>
2019-11-24 22:10:14 +0100

    Add all changes made

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const slider = document.querySelector('.pages');
+let isDown = false;
+let startY;
+let scrollDown;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startY = e.pageY - slider.offsetTop;
+  scrollTop = slider.scrollTop;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const y = e.pageY - slider.offsetTop;
+  const walk = (y - startY) * 3; //scroll-fast
+  slider.scrollTop = scrollDown - walk;
+  console.log(walk);
+});
---
+++ src/slider.js
@@ -0,0 +1,5 @@
+$( function() {
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical"
+    });
+} );
